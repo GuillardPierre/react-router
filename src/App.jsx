a6d6c25a32b1301f6a13ed1ebffbd29ae8d6c8a9
Pierre Guillard <pguillard95@gmail.com>
2025-07-21 11:58:15 +0200

    add NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 import Home from './pages/Home';
 import Layout from './components/Layout';
 import Category from './pages/Category';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route element={<Layout />}>
         <Route path='/' element={<Home />} />
         <Route path='categorie/:category' element={<Category />} />
+        <Route path='*' element={<NotFound />} />
       </Route>
     </Routes>
   );
