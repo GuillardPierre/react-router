4f38d29aeb562cabd19d750a6fcae6f6edf48f47
Pierre Guillard <pguillard95@gmail.com>
2025-07-21 14:35:44 +0200

    add header tag

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,23 +4,25 @@ import '../styles/header.scss';
 
 export default function Header() {
   return (
-    <Navbar expand='lg' className='bg-body-tertiary '>
-      <Container>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav'>
-          <Nav className='ms-auto'>
-            <Nav.Link as={NavLink} to='/'>
-              Accueil
-            </Nav.Link>
-            <Nav.Link as={NavLink} to='categorie/vêtements'>
-              Vêtements
-            </Nav.Link>
-            <Nav.Link as={NavLink} to='categorie/chaussures'>
-              Chaussures
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <header>
+      <Navbar expand='lg' className='bg-body-tertiary '>
+        <Container>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              <Nav.Link as={NavLink} to='/'>
+                Accueil
+              </Nav.Link>
+              <Nav.Link as={NavLink} to='categorie/vêtements'>
+                Vêtements
+              </Nav.Link>
+              <Nav.Link as={NavLink} to='categorie/chaussures'>
+                Chaussures
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
   );
 }
