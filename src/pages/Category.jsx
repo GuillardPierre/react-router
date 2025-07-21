2a177bf1e05a536d6c0b54a3803120aae846ef2f
Pierre Guillard <pguillard95@gmail.com>
2025-07-21 16:35:50 +0200

    added button and redirection to homepage with useNavigate

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,12 +1,20 @@
-import { Col, Container, Row } from 'react-bootstrap';
-import { useParams } from 'react-router';
+import { Button, Col, Row } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router';
 
 export default function Category() {
   const { category } = useParams();
+  const navigate = useNavigate();
+
+  const redirect = () => navigate('/');
 
   return (
-    <Row>
-      <Col as={'h1'}>{category}</Col>
+    <Row className='align-items-center'>
+      <Col>
+        <h1 className='mb-0'>{category}</h1>
+      </Col>
+      <Col className='text-end'>
+        <Button onClick={redirect}>Retour à la page d'accueil</Button>
+      </Col>
     </Row>
   );
 }
